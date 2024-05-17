0c656f202eee8c6c5ca73abf4d77b1f0fbcc9e62
manasranjan <ranjanmanas.z910@gmail.com>
2024-05-17 19:49:35 +0530

    compound interest added

--- src/main/java/org/example/SimpleCalculator.java
+++ src/main/java/org/example/SimpleCalculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SimpleCalculator {
 
-    public static double calculateSimpleInterest(double principal, double rate, double time) {
+    public static double calculateCompoundInterest(double principal, double rate, double time) {
         // Simple Interest formula: SI = (P * R * T) / 100
         double simpleInterest = (principal * rate * time) / 100;
         return simpleInterest;
